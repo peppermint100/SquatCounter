af958d108b35509a241f25d4febaa3f408b62c34
peppermint100 <krpeppermint100@gmail.com>
2024-07-09 22:03:45 +0900

    chore: R.Swift 세팅

--- SquatCounter/ContentView.swift
+++ SquatCounter/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(Color(uiColor: R.color.accentColor()!))
             Text("Hello, world!")
         }
         .padding()
