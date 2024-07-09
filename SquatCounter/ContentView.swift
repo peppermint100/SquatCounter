f4b03b62a8a1b8b16c9df4a9a009fc2a8881e7cf
peppermint100 <krpeppermint100@gmail.com>
2024-07-09 19:59:19 +0900

    gitigore

---
+++ SquatCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SquatCounter
+//
+//  Created by peppermint100 on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SquatCounter/SquatCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquatCounterApp.swift
+//  SquatCounter
+//
+//  Created by peppermint100 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SquatCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
